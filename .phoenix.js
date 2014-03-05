f01c86ea1475c6f62c03282c889903011833a777
Emil Pirfalt <emil.pirfalt@gmail.com>
2014-03-05 13:25:52 +0100

    add start for focus stack

--- .phoenix.js
+++ .phoenix.js
@@ -3,24 +3,17 @@
 
 // HELPERS
 
-// Sometimes it's easier to work with edges instead of point + size
-// {x, y, height, width} => {top, bottom, left right}
-function frameToRect(frame) {
-  return {
-    left: frame.x,
-    right: frame.x + frame.width,
-    top: frame.y,
-    bottom: frame.y + frame.height
+var _slice = Array.prototype.slice
+function noop() {}
+function Scaner(init, fn) {
+  return function scaner(next) {
+    return init = fn(init, next)
   }
 }
-
-// {top, bottom, left right} => {x, y, height, width}
-function rectToFrame(rect) {
-  return {
-    x: rect.left,
-    y: rect.top,
-    width: rect.right - rect.left,
-    height: rect.bottom - rect.top
+function invoke(name, args) {
+  if (Array.isArray(args)) args = _slice.call(arguments, 1)
+  return function invoker(obj) {
+    return obj[name].apply(obj, args)
   }
 }
 
@@ -33,10 +26,18 @@ function cycleCalls(fn, argsList) {
   return function () {
     if (lastCycle !== identifier || ++argIndex >= argsList.length) argIndex = 0
     lastCycle = identifier
-    fn.apply(this, argsList[argIndex])
+    return fn.apply(this, argsList[argIndex])
   }
 }
 
+function logString(obj) {
+  _.compose(api.alert, JSON.stringify)(obj)
+  return obj
+}
+
+function windowStack() {
+  return Window.visibleWindowsMostRecentFirst().filter(invoke('isNormalWindow'))
+}
 
 // ACTIONS
 
@@ -47,40 +48,54 @@ function toLeft(fillCols, maxCols) {
   var screen = win.screen().frameWithoutDockOrMenu()
   frame.width = screen.width * (fillCols / maxCols)
   frame.x = screen.x
-  win.setFrame(frame, win.screen())
+  return win.setFrame(frame, win.screen())
 }
-
 function toRight(fillCols, maxCols) {
   var win = Window.focusedWindow()
   var frame = win.frame()
   var screen = win.screen().frameWithoutDockOrMenu()
   frame.width = screen.width * (fillCols / maxCols)
   frame.x = screen.x + screen.width - frame.width
-  win.setFrame(frame, win.screen())
+  return win.setFrame(frame, win.screen())
 }
-
 function toTop(fillRows, maxRows) {
   var win = Window.focusedWindow()
   var frame = win.frame()
   var screen = win.screen().frameWithoutDockOrMenu()
   frame.height = screen.height * (fillRows / maxRows)
   frame.y = screen.y
-  win.setFrame(frame, win.screen())
+  return win.setFrame(frame, win.screen())
 }
-
 function toBottom(fillRows, maxRows) {
   var win = Window.focusedWindow()
   var frame = win.frame()
   var screen = win.screen().frameWithoutDockOrMenu()
   frame.height = screen.height * (fillRows / maxRows)
   frame.y = screen.y + screen.height - frame.height
-  win.setFrame(frame, win.screen())
+  return win.setFrame(frame, win.screen())
 }
 
+function focusIndex(idx) {
+  var wins = windowStack()
+  if (idx >= wins.length) idx = wins.length - 1
+  wins[idx].focusWindow()
+  return true
+}
 
 // BINDS
 var opts = ['ctrl', 'cmd']
 var optsShift = ['ctrl', 'cmd', 'shift']
+
+api.bind('u', opts, function () {
+
+})
+
+api.bind('i', opts, function () {
+})
+
+
+var cycleCalls
+
 api.bind('h', opts, cycleCalls(
   toLeft,
   [
@@ -115,5 +130,27 @@ api.bind('j', opts, cycleCalls(
     [1,3]
   ]
 ))
+
+
+
+api.bind('h', optsShift, function () {
+  var win = Window.focusedWindow()
+  win.focusWindowLeft()
+  return true
+})
+api.bind('j', optsShift, function () {
+  focusIndex(1000) // Massive hack, use (index out of range) => last.
+  return true
+})
+api.bind('k', optsShift, function () {
+  focusIndex(1)
+  return true
+})
+api.bind('l', optsShift, function () {
+  var win = Window.focusedWindow()
+  win.focusWindowRight()
+  return true
+})
+
 
 })()
